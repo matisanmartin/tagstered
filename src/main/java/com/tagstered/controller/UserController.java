95590b7270b293d81cd59b52f59826485a8e068e
Matias San Martin Acosta <msanmartinacosta@gmail.com>
2017-07-09 20:34:41 -0300

    Added logging and call to service on GET

--- src/main/java/com/tagstered/controller/UserController.java
+++ src/main/java/com/tagstered/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.tagstered.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tagstered.exception.TagsteredBusinessException;
 import com.tagstered.model.User;
 import com.tagstered.service.UserService;
 
@@ -18,6 +20,11 @@ import com.tagstered.service.UserService;
  */
 @RestController
 public class UserController {
+	
+	/**
+	 * 
+	 */
+	private static final Logger logger = Logger.getLogger(UserController.class);
 
 	/**
 	 * 
@@ -39,7 +46,12 @@ public class UserController {
 	 */
 	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> user(@PathVariable("id") String userId) {
-		return new ResponseEntity<User>(userService.findById(userId), HttpStatus.OK);
+		try {
+			return new ResponseEntity<User>(userService.findByUserId(userId), HttpStatus.OK);
+		} catch (TagsteredBusinessException e) {
+			logger.error("Error occured when retrieving user with userId: " + userId, e);
+			return new ResponseEntity<User>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 }
